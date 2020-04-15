40b69a0f24a470f9c8a4d7926744c18fde9c144c
yannickkk <39738426+yannickkk@users.noreply.github.com>
2020-04-15 14:24:00 +0200

    upload files from local shiny project

---
+++ global.R
@@ -0,0 +1,9 @@
+library(sf)
+library(foreign)
+library(RPostgreSQL)
+library(xlsx)
+library(openxlsx)
+library(reshape)
+
+source("C:/Users/ychaval/Documents/BD_Gardouch/Programmes/R/con_serveur_dbgardouch.R")
+source("C:/Users/ychaval/Documents/BD_tools/Mes_fonctions_R/fonctions.R")
