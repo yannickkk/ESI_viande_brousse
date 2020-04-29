d44ad701eaa388e3ccb0aea8e07a28fa2707a21b
VincentBlase <vincent.blase20@gmail.com>
2020-04-29 11:50:57 +0200

    Update 29/04

--- ui.R
+++ ui.R
@@ -2,31 +2,35 @@
 
 # Define UI for application 
 ui <- navbarPage("ESI-Congo",
-                 tabPanel("Dataviewer"),
-                 tabPanel("Spatial viewer"),
-                 tabPanel("Protocole"),
-                 tabPanel("Import dataset"),
-                 
-                 ######Sidebar Layout######
-        
-                 sidebarLayout(
-                   sidebarPanel(
-                     selectInput("var1",label = "Taxonomic rank",choices = list("choice 1" = 1, "choice 2" = 2), selected = 1),
-                     selectInput("var2",label = "Taxa (single or multiple)",choices = list("single" = 1, "multipe" = 2),selected = 1),
-                     selectInput("var3",label= "Market (single or multiple)",choices = list("Marché Central" = "Marché Central", "Marché Faubourg" = "Marché Faubourg","Marché Kouikou" = "Marché Kouikou","Marché Liberté" = "Marché Liberté","Marché Mayaka" = "Marché Mayaka","Marché Mvoumvou" = "Marché Mvoumvou","Marché Nkouikou" = "Marché Nkouikou", "Marché Tié-Tié" = "Marché Tié-Tié", "Tous les marchés" = "Tous les marchés"),selected = "Tous les marchés"),
-                     checkboxInput("checkbox", label = "Display weighted data : Number of bush meat occurences divided by number of visits", value = FALSE),
-                     dateRangeInput("dates", label = "Date range",start = "2008-01-01", end = "2019-12-31", min = "2008-01-01",max = "2019-12-31", format = 'yyyy', startview = "decade"),
-                   ),
-                   ##########################
-                   
-                   ######Main Panel######
-                   mainPanel( 
-                     plotlyOutput("plotly"),
-                   )
-                   ######################
-                 ),
-                 ####Bas de page####
-                 DT::dataTableOutput("DT")
-                 ###################
+                 tabPanel("Dataviewer",
+                          ######Sidebar Layout######
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput("rank",label = "Taxonomic rank",choices = list("Nom latin" = "NOM.LATIN", "class" = "class", "order"="order","family"="family","genus"="genus")),
+                              selectInput("taxa","Choose species",data$NOM.LATIN,multiple = TRUE),
+                              selectInput("var3",label= "Market (single or multiple)",choices = c("Tous les marchés", levels(data$LIEU)),selected = "Tous les marchés",multiple=TRUE),
+                              checkboxInput("checkbox", label = "Display weighted data : Number of bush meat occurences divided by number of visits", value = FALSE),
+                              checkboxInput("checkboxlog", label = "Display logarithmic scale for y axis", value = FALSE),
+                              dateRangeInput("dates", label = "Date range",start = "2008-01-01", end = "2019-12-31", min = "2008-01-01",max = "2019-12-31", format = 'yyyy', startview = "decade")
+                              ),
+                            ##########################
+                            ######Main Panel######
+                            mainPanel( 
+                              plotlyOutput("plotly")
+                              )
+                            ###################
+                            ),
+                          DT::dataTableOutput("DT")
+                          ),
+                 tabPanel("Species presentation",
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput("species",label = "Species",choices = list("Gorilla gorilla" = "Gorilla gorilla", "Hyemoschus aquaticus" = "Hyemoschus aquaticus", "Varanus niloticus" = "Varanus niloticus"), selected = "Gorilla gorilla")
+                              ),
+                            mainPanel(
+                              htmlOutput("frame")
+                            )
+                          )
+                        )
 )
 
